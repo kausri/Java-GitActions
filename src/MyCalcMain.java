665bec2e2a88436e9649fdb0e1c0796d9546ae6f
kausri <srinivas.kaushik@gmail.com>
2021-04-17 16:26:12 -0600

    Delete MyCalcMain.java

--- src/MyCalcMain.java
+++
@@ -1,9 +0,0 @@
-public class MyCalcMain {
-    public static void main(String[] args){
-        CalcClassForJunit myCalc = new CalcClassForJunit(18, 36);
-        System.out.println("Sum = " + myCalc.sumThemUp());
-        System.out.println("Dif = " + myCalc.subThemUp());
-        System.out.println("Product = " + myCalc.mulThemUp());
-        System.out.println("Div = " + myCalc.divThemUp());
-    }
-}
